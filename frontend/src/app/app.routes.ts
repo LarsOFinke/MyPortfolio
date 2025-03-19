435b2fca63ff545991a2620a6ab190a15658a022
Lars-Oliver Finke <dev.Lars-Finke@web.de>
2025-03-19 15:46:52 +0100

    Fully implemented the current HTML+CSS and started implementing the routing

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,4 +1,12 @@
 import { Routes } from '@angular/router';
 import { AboutMeComponent } from './components/about-me/about-me.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { QualificationsComponent } from './components/qualifications/qualifications.component';
 
-export const routes: Routes = [{ path: '', component: AboutMeComponent }];
+export const routes: Routes = [
+  { path: '', component: AboutMeComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'qualifications', component: QualificationsComponent },
+  { path: 'contact', component: ContactComponent },
+];
