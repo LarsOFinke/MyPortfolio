105b645289a32497b54b39eec66a6772bca28a1d
Lars-Oliver Finke <dev.Lars-Finke@web.de>
2025-03-19 15:42:47 +0100

    Implemented the About-Me-Page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
+import { AboutMeComponent } from './components/about-me/about-me.component';
 
-export const routes: Routes = [
-
-];
+export const routes: Routes = [{ path: '', component: AboutMeComponent }];
