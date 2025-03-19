c11855c4d6e8499ac10aba61aa58c5f4cb3c8d44
Lars-Oliver Finke <dev.Lars-Finke@web.de>
2025-03-19 17:28:42 +0100

    Splitted the navbar into mobile and desktop component each for better maintainability

---
+++ frontend/src/app/components/shared/navbar/mobile-navbar/mobile-navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mobile-navbar',
+  imports: [],
+  templateUrl: './mobile-navbar.component.html',
+  styleUrl: './mobile-navbar.component.css'
+})
+export class MobileNavbarComponent {
+
+}
--- frontend/src/app/components/shared/navbar/navbar.component.ts
+++ frontend/src/app/components/shared/navbar/navbar.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { MobileNavbarComponent } from './mobile-navbar/mobile-navbar.component';
+import { DesktopNavbarComponent } from './desktop-navbar/desktop-navbar.component';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [MobileNavbarComponent, DesktopNavbarComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
