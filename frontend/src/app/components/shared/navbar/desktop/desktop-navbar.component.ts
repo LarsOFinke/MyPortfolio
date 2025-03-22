c517c046ee9cd565e044eb4f160f5ee6db6d38cb
Lars-Oliver Finke <dev.Lars-Finke@web.de>
2025-03-22 13:47:57 +0100

    Further separated the navbar components.

--- frontend/src/app/components/shared/navbar/desktop/desktop-navbar.component.ts
+++ frontend/src/app/components/shared/navbar/desktop/desktop-navbar.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { NavLinkListComponent } from './nav-link-list/nav-link-list.component';
 
 @Component({
   selector: 'app-desktop-navbar',
-  imports: [],
+  imports: [NavLinkListComponent],
   templateUrl: './desktop-navbar.component.html',
   styleUrls: ['./desktop-navbar.component.css', '../navbar.component.css'],
 })
