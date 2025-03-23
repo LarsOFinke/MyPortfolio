e069c4665da5e80e3f4345d84702f6ff929881c4
Lars-Oliver Finke <dev.Lars-Finke@web.de>
2025-03-23 11:40:10 +0100

    Implemented variable acces for the contact-form for sending it to the backend - to be tested.

--- frontend/src/app/components/contact/contact.component.ts
+++ frontend/src/app/components/contact/contact.component.ts
@@ -1,9 +1,32 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css',
 })
-export class ContactComponent {}
+export class ContactComponent {
+  gender: string = '';
+  firstName: string = '';
+  lastName: string = '';
+  phone: string = '';
+  email: string = '';
+  companyName: string = '';
+  message: string = '';
+  tosAccepted: boolean = false;
+
+  submitContactData() {
+    const payload = {
+      gender: this.gender,
+      firstName: this.firstName,
+      lastName: this.lastName,
+      phone: this.phone,
+      email: this.email,
+      companyName: this.companyName,
+      message: this.message,
+      tosAccepted: this.tosAccepted
+    }
+  }
+}
