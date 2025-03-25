758c0b4779c2ad5c6221e7be856d737c310c9699
Lars-Oliver Finke <dev.Lars-Finke@web.de>
2025-03-25 17:24:36 +0100

    Navbar header is now modular, depending on the current route

--- frontend/src/app/components/navbar/mobile/dropdown/dropdown.component.ts
+++ frontend/src/app/components/navbar/mobile/dropdown/dropdown.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-dropdown',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './dropdown.component.html',
   styleUrl: './dropdown.component.css',
 })
--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,28 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MobileNavbarComponent } from './mobile/mobile-navbar.component';
 import { DesktopNavbarComponent } from './desktop/desktop-navbar.component';
-import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   imports: [MobileNavbarComponent, DesktopNavbarComponent],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css',
+  styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   header: string = 'TEST';
-  path!: string;
+  path: string = '';
 
-  constructor(private activatedRoute: ActivatedRoute) {}
+  constructor(private router: Router) {}
 
   ngOnInit() {
-    this.activatedRoute.url.subscribe((data: any) => (this.path = data.path));
+    // Subscribe to the router events to track the current route
+    this.router.events.subscribe((event: any) => {
+      // Only proceed when the navigation is 'end'
+      if (event?.urlAfterRedirects) {
+        this.path = event.urlAfterRedirects.split('/')[1] || ''; // Extract the path part
+        this.updateHeaderBasedOnRoute();
+      }
+    });
+  }
+
+  // Method to update the header based on the current path
+  private updateHeaderBasedOnRoute() {
     switch (this.path) {
       case '':
-        this.header = 'HOME';
+        this.header = 'HOME';  // Root path
         break;
 
       case 'projects':
+        this.header = 'PROJECTS';
+        break;
+
+      default:
+        this.header = this.path.toUpperCase() || 'UNKNOWN'; // Handle unknown routes
+        break;
     }
   }
 }
