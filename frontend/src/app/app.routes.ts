432645841f8449dfebfe2e0b6d4d7aef2d843ef5
Lars-Oliver Finke <dev.Lars-Finke@web.de>
2025-03-31 15:15:09 +0200

    Added impressum-component and linked it in the footer

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { AboutMeComponent } from './components/about-me/about-me.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { QualificationsComponent } from './components/qualifications/qualifications.component';
+import { ImpressumComponent } from './components/impressum/impressum.component';
 
 export const routes: Routes = [
   { path: '', component: AboutMeComponent },
   { path: 'projects', component: ProjectsComponent },
   { path: 'qualifications', component: QualificationsComponent },
   { path: 'contact', component: ContactComponent },
+  {path: 'impressum', component: ImpressumComponent}
 ];
