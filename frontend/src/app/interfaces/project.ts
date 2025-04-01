26f65722e6410e40199357ba59be5cf842f32933
Lars-Oliver Finke <dev.Lars-Finke@web.de>
2025-04-01 17:58:03 +0200

    Added the @for-loop for projects instead of hard-coding it in HTML

---
+++ frontend/src/app/interfaces/project.ts
@@ -0,0 +1,9 @@
+export interface Project {
+  id: number;
+  title: string,
+  websiteLink: string;
+  websiteTitle: string;
+  gitHubLink: string;
+  gitHubTitle: string;
+  descriptionText: string;
+}
