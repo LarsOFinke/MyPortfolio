e5c4010217229fce84735363dec65e6cca1d2445
Lars-Oliver Finke <dev.Lars-Finke@web.de>
2025-04-01 11:19:45 +0200

    Linkedin-logo-component done now.

--- frontend/src/app/components/qualifications/qualifications.component.ts
+++ frontend/src/app/components/qualifications/qualifications.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { UdemyQualificationsComponent } from './udemy-qualifications/udemy-qualifications.component';
 import { LinkedinQualificationsComponent } from './linkedin-qualifications/linkedin-qualifications.component';
+import { LinkedinLogoComponent } from '../shared/logos/linkedin-logo/linkedin-logo.component';
 
 @Component({
   selector: 'app-qualifications',
-  imports: [UdemyQualificationsComponent, LinkedinQualificationsComponent],
+  imports: [UdemyQualificationsComponent, LinkedinLogoComponent, LinkedinQualificationsComponent],
   templateUrl: './qualifications.component.html',
   styleUrl: './qualifications.component.css',
 })
