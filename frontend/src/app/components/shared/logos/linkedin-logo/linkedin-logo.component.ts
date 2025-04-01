d4345d037ad7b4a2858922ddb01654738ab5cd6e
Lars-Oliver Finke <dev.Lars-Finke@web.de>
2025-04-01 11:21:34 +0200

    Refactored the linkedin-logo

--- frontend/src/app/components/shared/logos/linkedin-logo/linkedin-logo.component.ts
+++ frontend/src/app/components/shared/logos/linkedin-logo/linkedin-logo.component.ts
@@ -7,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrl: './linkedin-logo.component.css'
 })
 export class LinkedinLogoComponent {
-
+  myLinkedinLink: string = 'https://www.linkedin.com/in/lars-oliver-finke-35a65a347/';
+  linkedinLogoName: string = 'linkedin.png';
 }
