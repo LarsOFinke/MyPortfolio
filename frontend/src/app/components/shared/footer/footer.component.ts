5ac99e521e1861e2d32d83e4127121135a1af7c2
Lars-Oliver Finke <dev.Lars-Finke@web.de>
2025-04-01 12:19:34 +0200

    Footer now has the logos for linkedin and github, removed from projects and qualifications pages.

--- frontend/src/app/components/shared/footer/footer.component.ts
+++ frontend/src/app/components/shared/footer/footer.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { LinkedinLogoComponent } from '../logos/linkedin-logo/linkedin-logo.component';
+import { GitHubLogoComponent } from '../logos/git-hub-logo/git-hub-logo.component';
 
 @Component({
   selector: 'app-footer',
-  imports: [RouterLink],
+  imports: [RouterLink, GitHubLogoComponent, LinkedinLogoComponent],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css',
 })
