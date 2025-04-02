c71fb9b10c6a8449046248afc6a9d82a985129ae
Lars-Oliver Finke <dev.Lars-Finke@web.de>
2025-04-02 16:32:49 +0200

    Refactored the about-me- and impressum-page to use data binding for the texts

--- frontend/src/app/components/about-me-page/about-me-page.component.ts
+++ frontend/src/app/components/about-me-page/about-me-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-about-me-page',
@@ -8,4 +9,51 @@ import { Component } from '@angular/core';
 })
 export class AboutMePageComponent {
   profilePicture: string = 'profilbild.jpg';
+  teaserText: string = `Ich bin Lars-O. Finke, Jahrgang '97 und wohnhaft in der schönen Hansestadt Hamburg!`;
+  aboutMeText: SafeHtml = `<p>
+        Im Laufe des letzten Jahres konnte ich autodidaktisch einen guten und
+        tiefen Einblick in das Berufsfeld eines Software Developers gewinnen und
+        habe durch die aktuell laufende Umschulung dort mein "neues Zuhause"
+        gefunden.
+      </p>
+
+      <p>
+        Zu meiner Person ist zu sagen, dass ich ein weltoffener und sehr
+        neugieriger Mensch bin, der schon seit jeher ein ausgeprägtes Interesse
+        im Bereich Technik hat.
+      </p>
+
+      <p>
+        Bereits in meiner Kindheit hatte ich steten Kontakt mit Computern &
+        Konsolen und bin bis heute sehr aktiv in der Online-Szene unterwegs,
+        verbringe quasi jede freie Minute am PC.
+      </p>
+
+      <p>
+        Ich verfüge über sehr gute Englischkenntnisse, besitze ein ausgeprägtes
+        logisches Denken, eine zumeist schnelle Auffassungsgabe und ein sehr
+        hohes Maß an Eigeninitiative, wenn es darum geht etwas Neues zu erlernen
+        oder ein Problem zu lösen.
+      </p>
+
+      <p>
+        Desweiteren liebe ich es mein Wissen mit meinen Mitmenschen zu teilen,
+        weswegen man mich oft die "wandelnde Bibliothek" nennt.
+      </p>
+
+      <p>
+        Mein Ziel ist es ein Standbein in dem Bereich aufzubauen, den ich liebe
+        und in dem ich mich wohl fühle. <br />
+        Langfristig will ich einen Ausbilderschein machen, um meiner Berufung
+        einen "tieferen Sinn" zu geben und auf diese Art meinen Fußabdruck für
+        die zukünftigen Generationen zu hinterlassen.
+      </p>
+
+      <p>
+        Wenn Sie also auf der Suche nach einem selbständigen Menschen sind, der
+        nicht lange um den heißen Brei redet, klare Ziele und eine Menge
+        Engagement mitbringt...
+      </p>
+
+      <h3>Dann bin ich Ihr Mann!</h3>`
 }
--- frontend/src/app/components/impressum-page/impressum-page.component.ts
+++ frontend/src/app/components/impressum-page/impressum-page.component.ts
@@ -10,6 +10,14 @@ import { currentEnvironment } from '../../../environment/environment';
 })
 export class ImpressumPageComponent implements OnInit {
   contactInformation: SafeHtml = '';
+  haftungsausschluss: String = `Alle Inhalte dieser Webseite wurden mit größter Sorgfalt erstellt.
+                                Für die Richtigkeit, Vollständigkeit und Aktualität der Inhalte
+                                kann jedoch keine Gewähr übernommen werden.`;
+  urheberrecht: String = `Die auf dieser Webseite veröffentlichten Inhalte, Werke und Beiträge
+                          sind urheberrechtlich geschützt. Jede Verwertung, insbesondere die
+                          Vervielfältigung, Verarbeitung, Verbreitung oder Speicherung außerhalb
+                          der Grenzen des Urheberrechts, bedarf der vorherigen schriftlichen
+                          Zustimmung des/der jeweiligen Rechteinhaber(s).`;  
 
   constructor(private sanitizer: DomSanitizer) {}
 
