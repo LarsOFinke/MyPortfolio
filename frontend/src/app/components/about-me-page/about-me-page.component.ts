d3d2ba3b871695448d20827970fe47389d53b1f6
Lars-Oliver Finke <dev.Lars-Finke@web.de>
2025-04-03 19:19:01 +0200

    Updated the about-me-text

--- frontend/src/app/components/about-me-page/about-me-page.component.ts
+++ frontend/src/app/components/about-me-page/about-me-page.component.ts
@@ -10,11 +10,21 @@ import { SafeHtml } from '@angular/platform-browser';
 export class AboutMePageComponent {
   profilePicture: string = 'profilbild.jpg';
   teaserText: string = `Ich bin Lars-O. Finke, Jahrgang '97 und wohnhaft in der schönen Hansestadt Hamburg!`;
-  aboutMeText: SafeHtml = `<p>
-        Im Laufe des letzten Jahres konnte ich autodidaktisch einen guten und
-        tiefen Einblick in das Berufsfeld eines Software Developers gewinnen und
-        habe durch die aktuell laufende Umschulung dort mein "neues Zuhause"
-        gefunden.
+  aboutMeText: SafeHtml = `
+      <p>
+        Durch meine Umschulung habe ich die Grundlagen der Programmierung in Python erlernt 
+        und habe mich durch Flask langsam in den Bereich Webdevelopement herangetastet.
+      </p>
+
+      <p>
+      Ich bin demnach mit den Grundlagen von HTML, CSS und JavaScript vertraut und habe mittlerweile sogar 
+      auf Angular (TypeScript) umgebaut, um einen Einblick in die professionelle Entwicklung zu bekommen 
+      und mich an die Projektstruktur zu gewöhnen.
+      </p>
+
+      <p>
+      Insgesamt konnte ich durch meine Projekte einen ersten Einblick in das Berufsfeld eines Software Developers gewinnen 
+      und hoffe durch das Praktikum, und die Umschulung im Gesamten, dort mein "neues Zuhause" zu finden.
       </p>
 
       <p>
@@ -37,11 +47,6 @@ export class AboutMePageComponent {
       </p>
 
       <p>
-        Desweiteren liebe ich es mein Wissen mit meinen Mitmenschen zu teilen,
-        weswegen man mich oft die "wandelnde Bibliothek" nennt.
-      </p>
-
-      <p>
         Mein Ziel ist es ein Standbein in dem Bereich aufzubauen, den ich liebe
         und in dem ich mich wohl fühle. <br />
         Langfristig will ich einen Ausbilderschein machen, um meiner Berufung
@@ -55,5 +60,5 @@ export class AboutMePageComponent {
         Engagement mitbringt...
       </p>
 
-      <h3>Dann bin ich Ihr Mann!</h3>`
+      <h3>Dann bin ich Ihr Mann!</h3>`;
 }
