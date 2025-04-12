e3ec867bea0f11c3622d20ff6a0f321f38e9c899
Lars-Oliver Finke <dev.Lars-Finke@web.de>
2025-04-12 19:52:47 +0200

    Updated the projects

--- frontend/src/app/components/projects-page/projects/projects.component.ts
+++ frontend/src/app/components/projects-page/projects/projects.component.ts
@@ -25,7 +25,7 @@ export class ProjectsComponent {
     },
     {
       id: 2,
-      title: 'Quiz Website',
+      title: 'Quiz Webseite',
       websiteLink: 'https://quizwebsite.portfolio-finke.de/',
       websiteTitle: 'Link zur Quiz Website.',
       gitHubLink: 'https://github.com/LarsOFinke/QuizWebsite',
@@ -35,6 +35,18 @@ export class ProjectsComponent {
                         HTML, CSS und JavaScript Frontend.`,
     },
     {
+      id: 2,
+      title: 'Quiz Website - Angular',
+      websiteLink: 'https://quizwebsite-angular.portfolio-finke.de/',
+      websiteTitle: 'Link zur Quiz Website - Angular.',
+      gitHubLink: 'https://github.com/LarsOFinke/QuizWebsite-Angular',
+      gitHubTitle: 'Link zum Quellcode auf GitHub für die Quiz Website - Angular',
+      descriptionText: `Dieses Projekt ist die Quiz Webseite erneut gebaut, dieses mal mit dem Angular-Framework. 
+                        Demnach wird TypeScript im Frontend verwendet, das Backend besteht weiterhin aus Python (Flask).
+                        Insbesondere die komponentenbasierte Projektstruktur und damit einhergehende Modularität hat mich
+                        überzeugt Angular erstmal als mein Haupt-Augenmerk zu wählen.`,
+    },
+    {
       id: 3,
       title: 'Eternal Galaxy Conquest',
       websiteLink: 'https://egc.portfolio-finke.de/',
@@ -42,7 +54,8 @@ export class ProjectsComponent {
       gitHubLink: 'https://github.com/LarsOFinke/EternalGalaxyConquest',
       gitHubTitle: 'Link zum Quellcode auf GitHub für Eternal Galaxy Conquest.',
       descriptionText: `Das Projekt besteht aus Python, HTML, CSS und JavaScript, wobei Flask als micro-Framework
-                        dient und den Websocket + API-Routen zur Verfügung stellt.
+                        dient und den Websocket + API-Routen zur Verfügung stellt. Es steht bis jetzt das Login-System
+                        und die Map-Generierung mit den Tile-Menüs und einigen Untermenüs.
                         Bei diesem Projekt bringt mich insbesondere die vertiefte 
                         Arbeit mit Javascript und einem Websocket dazu immer tiefer zu graben.`,
     },
